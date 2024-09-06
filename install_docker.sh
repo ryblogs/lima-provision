959aaef546cf06d2416678d74290d7bec7c52289
ryblogs <sullenberger@gmail.com>
2024-09-05 22:42:01 -0400

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+apt update
+apt upgrade -y
+apt install tree -y
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Add user to docker group
+usermod -aG docker {{.User}}
+# Create the marker file to indicate provisioning has been completed
+touch /var/lib/provision-done
+# Reboot the VM to apply group changes
+echo "Rebooting the VM to apply changes..."
+reboot
